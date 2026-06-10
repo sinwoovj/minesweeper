a34a88b52a99a2e74aab7a25a58315ddd7a59519
sinwoovj <sinwoov@naver.com>
2026-06-10 20:49:35 +0900

    타일, 폰트, UI, 캐릭터 업데이트

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    private Color selectedColor = Color.gray;
+    private Color defaultColor = Color.white;
+
+    public void ActiveUI(GameObject obj, bool isActive)
+    {
+        obj.SetActive(isActive);
+    }
+
+    public void SelectUI(List<Button> objs, Button obj)
+    {
+        foreach (Button o in objs)
+        {
+            o.Select();
+            //o.Equals(obj) ? true : false
+        }
+    }
+
+    public void MoveScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void GameExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
